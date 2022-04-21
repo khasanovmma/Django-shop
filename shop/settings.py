3a9073e226150a3401e556767c4ad77b8644d2ce
KhasanovMMA <KhasanovMMA010@gmail.com>
2022-04-21 10:34:57 +0500

    deploy v1.1

--- shop/settings.py
+++ shop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,8 +130,9 @@ INTERNAL_IPS = [
 ]
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join( BASE_DIR / 'static')
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    # BASE_DIR / 'static',
     BASE_DIR / 'store/static'
 ]
 
